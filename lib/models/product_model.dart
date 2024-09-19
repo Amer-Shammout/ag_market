559fac264aff6bb547715943bce28bdcd4b78c89
AmerShammout <amershammout2004@gmail.com>
2024-09-19 18:46:31 +0300

    create type adapter

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,7 +1,23 @@
+import 'package:hive/hive.dart';
+
+part 'product_model.g.dart';
+
+
+@HiveType(typeId: 0)
 class ProductModel {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final double price;
-  final String title, description, category, image;
+  @HiveField(2)
+  final String title;
+  @HiveField(3)
+  final String description;
+  @HiveField(4)
+  final String category;
+  @HiveField(5)
+  final String image;
+  @HiveField(6)
   final RatingModel rate;
 
   ProductModel( {
---
+++ lib/models/product_model.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductModelAdapter extends TypeAdapter<ProductModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  ProductModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProductModel(
+      id: fields[0] as int,
+      price: fields[1] as double,
+      title: fields[2] as String,
+      description: fields[3] as String,
+      category: fields[4] as String,
+      image: fields[5] as String,
+      rate: fields[6] as RatingModel,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProductModel obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.price)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.category)
+      ..writeByte(5)
+      ..write(obj.image)
+      ..writeByte(6)
+      ..write(obj.rate);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
